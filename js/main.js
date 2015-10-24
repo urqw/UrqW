4e03954492121e44384d8c9d4c955fbc41578064
narmiel <akela88@bk.ru>
2015-10-24 14:57:36 +0300

    зипы

--- js/main.js
+++ js/main.js
@@ -27,6 +27,8 @@ var mode;
 $(function() {
     $('#something_wrong').hide();
     $('#infopanel').show();
+    
+    $('#additionalstyle').find('style').empty();
 
     /**
      * Загрузить из хеша
@@ -38,59 +40,13 @@ $(function() {
         if (window.location.hash.length > 0) {
             JSZipUtils.getBinaryContent('quests/' + window.location.hash.substr(1) + '.zip', function(err, data) {
                 if(err) {
-                    throw err; // or handle err
+                    loadFromHashFailed();
                 }
 
-                var zip = new JSZip(data);
-
-
-                files = {};
-                var qst = null;
-
-                for (var i =0; i < zip.files.length; i++) {
-                    if (qst == null && zip.files[i].name.split('.').pop() == 'qst') {
-                        qst = zip.files[i];
-                    } else if (zip.files[i].name == 'style.css') {
-                        readStyle(zip.files[i]);
-                    } else {
-                        readFile(zip.files[i].name, zip.files[i]);
-                    }
-                }
-
-                if (!qst) {
-                    return;
-                }
-
-                // read file to global variable and start quest
-                var reader = new FileReader();
-                reader.onload = function() {
-                    mode = $('#urq_mode').val();
-                    setTimeout(function() {
-                        start(reader.result, qst.name);
-                    }, 200); // todo
-                };
-                reader.readAsText(qst, 'CP1251');
+                loadZip(data);
             });
         } else {
-            $.ajax({
-                url: 'games.json',
-                dataType: "json"
-            }).done(function(quests) {
-                for (var i = 0; i < quests.length; i++) {
-                    $('.gamelist').append(
-                        '<a href="#" class="list-group-item gamelink" data-game="' + quests[i].folder + '">' +
-                        '<div class="pull-right">' +
-                        '<span class="text-muted">' + quests[i].author + '</span>' +
-                        '</div>' +
-                        '<h4 class="list-group-item-heading">' + quests[i].title + '</h4>' +
-                        '<p class="list-group-item-text">' + quests[i].description + '</p>' +
-                        '</a>'
-                    );
-                }
-            });
-
-            $('#loading').hide();
-            $('#choose-game').show();
+            loadFromHashFailed();
         }
     }
 
@@ -98,6 +54,66 @@ $(function() {
      * Попробуем загрузить квест если в хеше что-то есть
      */
     loadFromHash();
+
+    
+    function loadZip(data) {
+        var zip = new JSZip(data);
+
+        files = {};
+        var qst = null;
+
+        for (var key in zip.files) {
+            if (!zip.files[key].dir) {
+                var file = zip.file(key);
+                if (qst == null && file.name.split('.').pop() == 'qst') {
+                    qst = file;
+                } else if (file.name.split('.').pop() == 'css') {
+                    $('#additionalstyle').find('style').append(file.asBinary());
+                } else {
+                    files[file.name] = URL.createObjectURL(new Blob([(file.asArrayBuffer())], {type: MIME[file.name.split('.').pop()]}));
+                }
+            }
+        }
+
+        if (qst) {
+            if (qst.name.lastIndexOf('/') != -1) {
+                var dir = qst.name.substring(0, qst.name.lastIndexOf('/') + 1);
+
+                for (var key in files) {
+                    var newkey = key.substr(dir.length);
+                    files[newkey] = files[key];
+                    delete files[key];
+                }
+            }
+
+            start(win2unicode(qst.asBinary()), qst.name);
+        }    
+    }
+    
+    /**
+     * 
+     */
+    function loadFromHashFailed() {
+        $.ajax({
+            url: 'games.json',
+            dataType: "json"
+        }).done(function(quests) {
+            for (var i = 0; i < quests.length; i++) {
+                $('.gamelist').append(
+                    '<a href="#" class="list-group-item gamelink" data-game="' + quests[i].folder + '">' +
+                    '<div class="pull-right">' +
+                    '<span class="text-muted">' + quests[i].author + '</span>' +
+                    '</div>' +
+                    '<h4 class="list-group-item-heading">' + quests[i].title + '</h4>' +
+                    '<p class="list-group-item-text">' + quests[i].description + '</p>' +
+                    '</a>'
+                );
+            }
+        });
+
+        $('#loading').hide();
+        $('#choose-game').show();
+    }
 
     /**
      * Выбор игры из списка
@@ -115,8 +131,21 @@ $(function() {
     $('#quest').on('change', function(e) {
         files = {};
         var qst = null;
+        var reader = new FileReader();
+        
+        if (e.target.files.length == 1 && e.target.files[0].name.split('.').pop() == 'zip') {
+            var zip = e.target.files[0];    
+            
+            reader.onload = function() {
+                mode = $('#urq_mode').val();
+                loadZip(reader.result);
+            };
+            reader.readAsBinaryString(zip, 'CP1251');
 
-        for (var i =0; i < e.target.files.length; i++) {
+            return;
+        }
+
+        for (var i = 0; i < e.target.files.length; i++) {
             if (qst == null && e.target.files[i].name.split('.').pop() == 'qst') {
                 qst = e.target.files[i];
             } else if (e.target.files[i].name == 'style.css') {
@@ -131,7 +160,6 @@ $(function() {
         }
 
         // read file to global variable and start quest
-        var reader = new FileReader();
         reader.onload = function() {
             mode = $('#urq_mode').val();
             setTimeout(function() {
@@ -148,9 +176,10 @@ $(function() {
     function readFile(filename, file) {
         var reader = new FileReader();
         reader.onload = function() {
-            files[filename] = reader.result;
+            files[filename] = URL.createObjectURL(new Blob([reader.result], {type: MIME[filename.split('.').pop()]}));
         };
-        reader.readAsDataURL(file);
+
+        reader.readAsArrayBuffer(file);
     }
 
     /**
@@ -159,7 +188,6 @@ $(function() {
     function readStyle(file) {
         var style = new FileReader();
         style.onload = function() {
-            $('#additionalstyle').find('style').empty();
             $('#additionalstyle').find('style').append(style.result);
         };
 
