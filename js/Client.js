489c260fc0d87b40982430dc5fb62d0799637d6a
narmiel <akela88@bk.ru>
2015-10-27 17:41:43 +0300

    всё на прототипы

--- js/Client.js
+++ js/Client.js
@@ -32,269 +32,269 @@ function Client() {
     this.crtlInventory;
 
     /**
-     * @type {Client}
-     */
-    var me = this;
-
-    /**
      * @type {boolean}
      */
-    var fistRender = true;
+    this.firstRender = true;
     
-    /**
-     * render
-     */
-    this.render = function (data) {
-        if (fistRender) {
-            if (Game.getVar('urq_mode') == 'dosurq') {
-                $('#additionalstyle').find('style').empty().append('#buttons .button {background-color: #000; color: #AAA;}');
-            }
-            
-            fistRender = false;
-        }
-        
-        var backColor = null;
-        if (isNaN(Game.getVar('style_backcolor'))) {
-            backColor = Game.getVar('style_backcolor');
-        } else if (Game.getVar('style_textcolor') > 0) {
-            var red = (Game.getVar('style_backcolor') >> 16) & 0xFF;
-            var green = (Game.getVar('style_backcolor') >> 8) & 0xFF;
-            var blue = Game.getVar('style_backcolor') & 0xFF;
+}
 
-            backColor = 'rgb(' + blue + ', ' + green  + ', ' + red + ')';
+
+/**
+ * render
+ */
+Client.prototype.render = function (data) {
+    if (this.firstRender) {
+        if (Game.getVar('urq_mode') == 'dosurq') {
+            $('#additionalstyle').find('style').empty().append('#buttons .button {background-color: #000; color: #AAA;}');
         }
 
-        $('body').css('background-color', backColor);
-        
-        me.drawText();
-        if (data.status == PLAYER_STATUS_END) {
-            me.drawButtons();
-            me.drawInventory();
-        } else if (data.status == PLAYER_STATUS_ANYKEY) {
-            this.crtlButtonField.hide();
-            this.crtlInfo.text('[нажмите любую клавишу]');
-            this.crtlInfo.show();
-        } else if (data.status == PLAYER_STATUS_INPUT) {
-            this.crtlButtonField.hide();
-            this.crtlInput.removeClass('has-error');
-            this.crtlInput.find('input').val('');
-            this.crtlInput.show();
-            this.crtlInput.find('input').focus();
-        } else if (data.status == PLAYER_STATUS_PAUSE) {
-            me.drawButtons();
-            me.drawInventory();
+        this.firstRender = false;
+    }
 
-            var wait = GlobalPlayer.inf;
-            setTimeout(function() {
-                if (GlobalPlayer.status == PLAYER_STATUS_PAUSE) {
-                    GlobalPlayer.continue();
-                }
-            }, wait);
-        } else if (data.status == PLAYER_STATUS_QUIT) {
-            this.crtlInfo.text('[игра закончена]');
-            this.crtlInfo.show();
+    var backColor = null;
+    if (isNaN(Game.getVar('style_backcolor'))) {
+        backColor = Game.getVar('style_backcolor');
+    } else if (Game.getVar('style_textcolor') > 0) {
+        var red = (Game.getVar('style_backcolor') >> 16) & 0xFF;
+        var green = (Game.getVar('style_backcolor') >> 8) & 0xFF;
+        var blue = Game.getVar('style_backcolor') & 0xFF;
+
+        backColor = 'rgb(' + blue + ', ' + green  + ', ' + red + ')';
+    }
+
+    $('body').css('background-color', backColor);
+
+    this.drawText();
+    if (data.status == PLAYER_STATUS_END) {
+        this.drawButtons();
+        this.drawInventory();
+    } else if (data.status == PLAYER_STATUS_ANYKEY) {
+        this.crtlButtonField.hide();
+        this.crtlInfo.text('[нажмите любую клавишу]');
+        this.crtlInfo.show();
+    } else if (data.status == PLAYER_STATUS_INPUT) {
+        this.crtlButtonField.hide();
+        this.crtlInput.removeClass('has-error');
+        this.crtlInput.find('input').val('');
+        this.crtlInput.show();
+        this.crtlInput.find('input').focus();
+    } else if (data.status == PLAYER_STATUS_PAUSE) {
+        this.drawButtons();
+        this.drawInventory();
+
+        var wait = GlobalPlayer.inf;
+        setTimeout(function() {
+            if (GlobalPlayer.status == PLAYER_STATUS_PAUSE) {
+                GlobalPlayer.continue();
+            }
+        }, wait);
+    } else if (data.status == PLAYER_STATUS_QUIT) {
+        this.crtlInfo.text('[игра закончена]');
+        this.crtlInfo.show();
+    }
+};
+
+/**
+ * очистка экрана
+ */
+Client.prototype.cls = function() {
+    this.clst();
+    this.clsb();
+};
+
+/**
+ * очистка текста
+ */
+Client.prototype.clst = function() {
+    this.crtlTextField.empty();
+};
+
+/**
+ * очистка кнопок
+ */
+Client.prototype.clsb = function() {
+    this.crtlButtonField.empty();
+};
+
+/**
+ * Нарисовать текст
+ */
+Client.prototype.drawText = function () {
+    var me = this;
+    var scrolled = false;
+
+    this.crtlTextField.empty();
+
+    $.each(GlobalPlayer.text, function(index, text) {
+        var div = $('<div>').html(text[0]);
+
+        if (div.find('*:not(a, s, b, small, span, q, i)').length == 0) {
+            div.addClass('text');
         }
-    };
 
-    /**
-     * очистка экрана
-     */
-    this.cls = function() {
-        this.clst();
-        this.clsb();
-    };
+        if (text[2] !== 0) {
+            div.css('color', text[2]);
+        }
 
-    /**
-     * очистка текста
-     */
-    this.clst = function() {
-        this.crtlTextField.empty();
-    };
-
-    /**
-     * очистка кнопок
-     */
-    this.clsb = function() {
-        this.crtlButtonField.empty();
-    };
-
-    /**
-     * Нарисовать текст
-     */
-    this.drawText = function () {
-        var scrolled = false;
-        
-        me.crtlTextField.empty();
-        
-        $.each(GlobalPlayer.text, function(index, text) {
-            var div = $('<div>').html(text[0]);
-
-            if (div.find('*:not(a, s, b, small, span, q, i)').length == 0) {
-                div.addClass('text');
-            }
-            
-            if (text[2] !== 0) {
-                div.css('color', text[2]);
-            }
-
-            div.find('img').each(function(index) {
-                var src = $(this).attr('src');
-                if (src && src.indexOf('http://') == -1 && src.indexOf('https://') == -1) {
-                    if (files === null) {
-                        $(this).attr('src', 'quests/' + Game.name + '/' + src);
-                    } else {
-                        $(this).attr('src', files[src]); // todo
-                    }
+        div.find('img').each(function(index) {
+            var src = $(this).attr('src');
+            if (src && src.indexOf('http://') == -1 && src.indexOf('https://') == -1) {
+                if (files === null) {
+                    $(this).attr('src', 'quests/' + Game.name + '/' + src);
+                } else {
+                    $(this).attr('src', files[src]); // todo
                 }
-            });
+            }
+        });
 
-            div.find('a.button').each(function(index) {
-                var action = $(this).attr('data-action');
-                if (GlobalPlayer.links[action] == null) {
-                    $(this).contents().unwrap();
-                }
-            });
-            
-            if (text.rendered == undefined) {
+        div.find('a.button').each(function(index) {
+            var action = $(this).attr('data-action');
+            if (GlobalPlayer.links[action] == null) {
+                $(this).contents().unwrap();
+            }
+        });
+
+        if (text.rendered == undefined) {
 //                div.css('opacity', 0.5);
+        }
+
+        me.crtlTextField.append(div);
+
+        if (text[1]) {
+            me.crtlTextField.append('<div class="clearfix">');
+        }
+
+        if (text.rendered == undefined) {
+            if (scrolled == false) {
+                scrolled = true;
+                $('html, body').animate({scrollTop: div.offset().top}, 500);
             }
-
-            me.crtlTextField.append(div);
-
-            if (text[1]) {
-                me.crtlTextField.append('<div class="clearfix">');
-            }
-
-            if (text.rendered == undefined) {
-                if (scrolled == false) {
-                    scrolled = true;
-                    $('html, body').animate({scrollTop: div.offset().top}, 500);
-                }
 
 //                div.animate({opacity:1}, 800);
-            }
-            
-            GlobalPlayer.text[index].rendered = true;
-        });
-    };
-
-    /**
-     * Нарисовать кнопки
-     */
-    this.drawButtons = function () {
-        me.crtlButtonField.empty();
-        
-        $.each(GlobalPlayer.buttons, function(index, button) {
-            if (button) {
-                var buttonCtrl = $('<button class="list-group-item button">').attr('data-action', button.id).html(button.desc);
-
-                me.crtlButtonField.append(buttonCtrl);
-            }
-        });
-    };
-
-    /**
-     * Нарисовать инвентарь
-     */
-    this.drawInventory = function () {
-        this.crtlInventory.empty();
-        this.crtlInventory.append(me.drawItem('inv', 1));
-
-        // обновляем список предметов
-        $.each(Game.items, function(itemName, quantity) {
-            me.crtlInventory.append(me.drawItem(itemName, quantity));
-        });
-
-        if (this.crtlInventory.find('> li').length == 0) {
-            this.crtlInventory.append('<li><a href="#" class="item_use">(Пусто)</a></li>');
         }
-    };
 
-    /**
-     * @param {String} itemName
-     * @param {int} quantity
-     * 
-     * @return {Jquery|String}
-     */
-    this.drawItem = function (itemName, quantity) {
+        GlobalPlayer.text[index].rendered = true;
+    });
+};
 
-        var actions = [];
+/**
+ * Нарисовать кнопки
+ */
+Client.prototype.drawButtons = function () {
+    var me = this;
+    this.crtlButtonField.empty();
 
-        $.each(Game.useLabels, function(index, value) {
-            if ((itemName.toLowerCase() + '_' == index.substr(4, itemName.length + 1).toLowerCase())
-            || (itemName.toLowerCase() == index.substr(4).toLowerCase())
-            ) {
-                var actionName = index.substr(itemName.length + 5);
+    $.each(GlobalPlayer.buttons, function(index, button) {
+        if (button) {
+            var buttonCtrl = $('<button class="list-group-item button">').attr('data-action', button.id).html(button.desc);
 
-                actions.push([actionName, index]);
-            } 
-        });
-
-        itemName = itemName.replace(/_/g, ' ');
-
-        if (actions.length == 0 && itemName != 'inv') {
-            if (quantity > 1) {
-                itemName = itemName + ' (' + quantity + ')';
-            }
-
-            return '<li><a href="#" class="item_use">' + itemName + '</a></li>';
-        } if (actions.length == 1 && itemName != 'inv' && actions[0][0] == '') {
-            if (quantity > 1) {
-                itemName = itemName + ' (' + quantity + ')';
-            }
-            
-            var a = $('<a href="#" class="item_use">').attr('data-label', actions[0][1]).text(itemName);
-            a.html('<span class="glyphicon glyphicon-cog"></span> ' + a.text());
-            
-            return $('<li>').append(a);
-        } else if (actions.length > 0)  {
-
-            if (itemName == 'inv') {
-                itemName = 'Инвентарь';
-            } else {
-                if (quantity > 1) {
-                    itemName = itemName + ' (' + quantity + ')';
-                }
-            }
-
-            var li = $('<li>').addClass('dropdown-submenu').append($('<a href="#" class="item_use">').text(itemName));
-            var ul = $('<ul class="dropdown-menu">');
-            var li2 = $('<li class="menu-item">');
-
-            for (var i = 0; i < actions.length; i++) {
-                if (actions[i][0] == '') {
-                    actions[i][0] = 'Осмотреть';
-                }
-                
-                li2.append($('<a href="#" class="item_use">').attr('data-label', actions[i][1]).text(actions[i][0]));
-            }
-
-            ul.append(li2);
-            li.append(ul);
-
-            return li;
+            me.crtlButtonField.append(buttonCtrl);
         }
-    };
+    });
+};
 
-    /**
-     * превратить текст и комманду в <a> тег
-     * @param {String} text
-     * @param {int} action
-     */
-    this.convertToLink = function(text, action) {
-        return "<a data-action='" + action + "' class='button'>" + text + "</a>";
-    };
+/**
+ * Нарисовать инвентарь
+ */
+Client.prototype.drawInventory = function () {
+    var me = this;
+    this.crtlInventory.empty();
+    this.crtlInventory.append(this.drawItem('inv', 1));
 
-    /**
-     * преврать текст вида <a ...>текст</a> в текст
-     * @param text 
-     */
-    this.disableLink = function(text) {
-        var a = $(text);
-        if (a.hasClass('button')) {
-            return a.text();
-        } else {
-            return text;
-        }
+    // обновляем список предметов
+    $.each(Game.items, function(itemName, quantity) {
+        me.crtlInventory.append(me.drawItem(itemName, quantity));
+    });
+
+    if (this.crtlInventory.find('> li').length == 0) {
+        this.crtlInventory.append('<li><a href="#" class="item_use">(Пусто)</a></li>');
     }
-}
+};
+
+/**
+ * @param {String} itemName
+ * @param {int} quantity
+ *
+ * @return {Jquery|String}
+ */
+Client.prototype.drawItem = function (itemName, quantity) {
+
+    var actions = [];
+
+    $.each(Game.useLabels, function(index, value) {
+        if ((itemName.toLowerCase() + '_' == index.substr(4, itemName.length + 1).toLowerCase())
+            || (itemName.toLowerCase() == index.substr(4).toLowerCase())
+        ) {
+            var actionName = index.substr(itemName.length + 5);
+
+            actions.push([actionName, index]);
+        }
+    });
+
+    itemName = itemName.replace(/_/g, ' ');
+
+    if (actions.length == 0 && itemName != 'inv') {
+        if (quantity > 1) {
+            itemName = itemName + ' (' + quantity + ')';
+        }
+
+        return '<li><a href="#" class="item_use">' + itemName + '</a></li>';
+    } if (actions.length == 1 && itemName != 'inv' && actions[0][0] == '') {
+        if (quantity > 1) {
+            itemName = itemName + ' (' + quantity + ')';
+        }
+
+        var a = $('<a href="#" class="item_use">').attr('data-label', actions[0][1]).text(itemName);
+        a.html('<span class="glyphicon glyphicon-cog"></span> ' + a.text());
+
+        return $('<li>').append(a);
+    } else if (actions.length > 0)  {
+
+        if (itemName == 'inv') {
+            itemName = 'Инвентарь';
+        } else {
+            if (quantity > 1) {
+                itemName = itemName + ' (' + quantity + ')';
+            }
+        }
+
+        var li = $('<li>').addClass('dropdown-submenu').append($('<a href="#" class="item_use">').text(itemName));
+        var ul = $('<ul class="dropdown-menu">');
+        var li2 = $('<li class="menu-item">');
+
+        for (var i = 0; i < actions.length; i++) {
+            if (actions[i][0] == '') {
+                actions[i][0] = 'Осмотреть';
+            }
+
+            li2.append($('<a href="#" class="item_use">').attr('data-label', actions[i][1]).text(actions[i][0]));
+        }
+
+        ul.append(li2);
+        li.append(ul);
+
+        return li;
+    }
+};
+
+/**
+ * превратить текст и комманду в <a> тег
+ * @param {String} text
+ * @param {int} action
+ */
+Client.prototype.convertToLink = function(text, action) {
+    return "<a data-action='" + action + "' class='button'>" + text + "</a>";
+};
+
+/**
+ * преврать текст вида <a ...>текст</a> в текст
+ * @param text
+ */
+Client.prototype.disableLink = function(text) {
+    var a = $(text);
+    if (a.hasClass('button')) {
+        return a.text();
+    } else {
+        return text;
+    }
+};
--- js/Quest.js
+++ js/Quest.js
@@ -54,218 +54,218 @@ function Quest(text) {
      * @type {string}
      */
     this.quest = text.replace(/^[\n\r]+|[\n\r]+$/g,'').replace(/\/\*[\s\S.]+?\*\//g,'').split(/[\n\r]+/);
-
-    /**
-     * @param {String} label
-     */
-    this.getLabel = function(label) {
-        label = label.toString().toLowerCase();
-
-        if (this.labels[label] != undefined) {
-            return {
-                name: label,
-                pos: this.labels[label]
-            };
-        } else {
-            return false;
-        }
-    };
-
-    /**
-     * следующая строка
-     */
-    this.next = function() {
-        var line = this.get(this.position);
-
-        this.position++;
-
-        if (!line) {
-            return false;
-        }
-
-        if (this.getVar('urq_mode') == 'ripurq' || this.getVar('urq_mode') == 'dosurq') {
-            // вырезать комментарий
-            if (line.indexOf(';') != -1) {
-                line = line.substring(0, line.indexOf(';'));
-            }
-        }
-
-        return line.replace(/\t/g, ' ');
-    };
-
-    /**
-     * строка по номеру
-     */
-    this.get = function(i) {
-        if (this.quest[i] != undefined) {
-            return this.quest[i];
-        } else  {
-            return false;
-        }
-    };
-
-    /**
-     * инициализация
-     */
-    this.init = function() {
-        this.labels = {};
-        this.useLabels = {};
-        this.items = {};
-        this.vars = {
-            'tokens_delim': '\ \,\"\?\!'
-        };
-        this.position = 0;
-        this.realCurrentLoc = '';
-
-        /**
-         * Собираем метки
-         */
-        for (var i = this.quest.length -1; i >=0; i--) {
-            var str = this.get(i);
-
-            if (str.substr(0, 1) == '_' && str.substr(1, 1) != '_') {
-                this.quest[i - 1] = this.quest[i - 1] + str.substr(1);
-                this.quest[i] = '';
-            } else if (str.substr(0, 1) == ':') {
-                if (str.substr(0, 5).toLowerCase() == ':use_') {
-                    this.useLabels[str.substr(1).toLowerCase().trim()] = i;
-                }
-
-                this.labels[str.substr(1).toLowerCase().trim()] = i;
-            }
-        }
-
-        for (var i = 0; i < this.quest.length; i++) {
-            if (this.quest[i].substr(0, 1) == ':') {
-                var key = this.quest[i].substr(1).toLowerCase().trim();
-                this.realCurrentLoc = key;
-                this.setVar('current_loc', key);
-                this.setVar('previous_loc', key);
-                
-                break;
-            }
-        }
-    };
-
-    /**
-     * @param name
-     * @param {int} count
-     */
-    this.addItem = function(name, count) {
-        return this.setItem(name, this.getItem(name) + parseInt(count));
-    };
-
-    /**
-     * @param name
-     * @param {int} count
-     */
-    this.removeItem = function(name, count) {
-        return this.setItem(name, this.getItem(name) - parseInt(count));
-    };
-
-    /**
-     *
-     * @param name
-     * @param {int} count
-     */
-    this.setItem = function (name, count) {
-        if (Game.locked) return false;
-
-        count = parseInt(count);
-
-        if (count <= 0) {
-            delete this.items[name];
-            this.setVar(name, 0);
-        } else {
-            this.items[name] = count;
-            this.setVar(name, count);
-        }
-    };
-
-    /**
-     *
-     * @param name
-     * @return {int}
-     */
-    this.getItem = function (name) {
-        return (this.items[name] == undefined) ? 0 : this.items[name];
-    };
-
-    /**
-     * @param {String} variable
-     * @param {*} value
-     */
-    this.setVar = function(variable, value) {
-        if (variable.substr(0, 4).toLowerCase() == 'inv_') {
-            variable = variable.substr(4);
-
-            this.setItem(variable, value);
-        } else {
-            this.vars[variable.toLowerCase()] = value;
-        }
-    };
-
-    /**
-     * @param variable
-     * @returns {*}
-     */
-    this.getVar = function(variable) {
-        variable = variable.toLowerCase();
-
-        if (variable.substr(0, 4) == 'inv_') {
-            variable = variable.substr(4);
-        }
-
-        if (variable == 'rnd') {
-            return Math.random();
-        } else if (variable.substr(0, 3) == 'rnd') {
-            return Math.floor(Math.random() * parseInt(variable.substr(3))) + 1;
-        }
-
-        // Для выражений вроде "1 деньги"
-        if (variable.split(' ').length > 1) {
-            var count = variable.split(' ')[0];
-            if (!isNaN(count)) {
-                variable = variable.split(' ').slice(1).join(' ').trim()
-                return this.vars[variable] >= count;
-            }
-        }
-
-        if (this.vars[variable] != undefined) {
-            return this.vars[variable];
-        }
-
-        return 0;
-    };
-
-    /**
-     * сохранение
-     *
-     * @param {int} slot
-     */
-    this.save = function(slot) {
-        var Datetime = new Date();
-
-        localStorage.setItem(this.name + '_' + slot.toString() + '_name', Datetime.toLocaleDateString() + ' ' + Datetime.toLocaleTimeString());
-        localStorage.setItem(this.name + '_' + slot.toString() + '_data', JSON.stringify({
-            items: this.items,
-            vars: this.vars,
-            position: this.position,
-            realCurrentLoc: this.realCurrentLoc
-        }));
-    };
-
-    /**
-     * загрузка
-     *
-     * @param {int} slot
-     */
-    this.load = function(slot) {
-        var data = JSON.parse(localStorage.getItem(this.name + '_' + slot.toString() + '_data'));
-        this.items = data.items;
-        this.vars = data.vars;
-        this.position = data.position;
-        this.realCurrentLoc = data.realCurrentLoc;
-    };
 }
 
+
+/**
+ * @param {String} label
+ */
+Quest.prototype.getLabel = function(label) {
+    label = label.toString().toLowerCase();
+
+    if (this.labels[label] != undefined) {
+        return {
+            name: label,
+            pos: this.labels[label]
+        };
+    } else {
+        return false;
+    }
+};
+
+/**
+ * следующая строка
+ */
+Quest.prototype.next = function() {
+    var line = this.get(this.position);
+
+    this.position++;
+
+    if (!line) {
+        return false;
+    }
+
+    if (this.getVar('urq_mode') == 'ripurq' || this.getVar('urq_mode') == 'dosurq') {
+        // вырезать комментарий
+        if (line.indexOf(';') != -1) {
+            line = line.substring(0, line.indexOf(';'));
+        }
+    }
+
+    return line.replace(/\t/g, ' ');
+};
+
+/**
+ * строка по номеру
+ */
+Quest.prototype.get = function(i) {
+    if (this.quest[i] != undefined) {
+        return this.quest[i];
+    } else  {
+        return false;
+    }
+};
+
+/**
+ * инициализация
+ */
+Quest.prototype.init = function() {
+    this.labels = {};
+    this.useLabels = {};
+    this.items = {};
+    this.vars = {
+        'tokens_delim': '\ \,\"\?\!'
+    };
+    this.position = 0;
+    this.realCurrentLoc = '';
+
+    /**
+     * Собираем метки
+     */
+    for (var i = this.quest.length -1; i >=0; i--) {
+        var str = this.get(i);
+
+        if (str.substr(0, 1) == '_' && str.substr(1, 1) != '_') {
+            this.quest[i - 1] = this.quest[i - 1] + str.substr(1);
+            this.quest[i] = '';
+        } else if (str.substr(0, 1) == ':') {
+            if (str.substr(0, 5).toLowerCase() == ':use_') {
+                this.useLabels[str.substr(1).toLowerCase().trim()] = i;
+            }
+
+            this.labels[str.substr(1).toLowerCase().trim()] = i;
+        }
+    }
+
+    for (var i = 0; i < this.quest.length; i++) {
+        if (this.quest[i].substr(0, 1) == ':') {
+            var key = this.quest[i].substr(1).toLowerCase().trim();
+            this.realCurrentLoc = key;
+            this.setVar('current_loc', key);
+            this.setVar('previous_loc', key);
+
+            break;
+        }
+    }
+};
+
+/**
+ * @param name
+ * @param {int} count
+ */
+Quest.prototype.addItem = function(name, count) {
+    return this.setItem(name, this.getItem(name) + parseInt(count));
+};
+
+/**
+ * @param name
+ * @param {int} count
+ */
+Quest.prototype.removeItem = function(name, count) {
+    return this.setItem(name, this.getItem(name) - parseInt(count));
+};
+
+/**
+ *
+ * @param name
+ * @param {int} count
+ */
+Quest.prototype.setItem = function (name, count) {
+    if (Game.locked) return false;
+
+    count = parseInt(count);
+
+    if (count <= 0) {
+        delete this.items[name];
+        this.setVar(name, 0);
+    } else {
+        this.items[name] = count;
+        this.setVar(name, count);
+    }
+};
+
+/**
+ *
+ * @param name
+ * @return {int}
+ */
+Quest.prototype.getItem = function (name) {
+    return (this.items[name] == undefined) ? 0 : this.items[name];
+};
+
+/**
+ * @param {String} variable
+ * @param {*} value
+ */
+Quest.prototype.setVar = function(variable, value) {
+    if (variable.substr(0, 4).toLowerCase() == 'inv_') {
+        variable = variable.substr(4);
+
+        this.setItem(variable, value);
+    } else {
+        this.vars[variable.toLowerCase()] = value;
+    }
+};
+
+/**
+ * @param variable
+ * @returns {*}
+ */
+Quest.prototype.getVar = function(variable) {
+    variable = variable.toLowerCase();
+
+    if (variable.substr(0, 4) == 'inv_') {
+        variable = variable.substr(4);
+    }
+
+    if (variable == 'rnd') {
+        return Math.random();
+    } else if (variable.substr(0, 3) == 'rnd') {
+        return Math.floor(Math.random() * parseInt(variable.substr(3))) + 1;
+    }
+
+    // Для выражений вроде "1 деньги"
+    if (variable.split(' ').length > 1) {
+        var count = variable.split(' ')[0];
+        if (!isNaN(count)) {
+            variable = variable.split(' ').slice(1).join(' ').trim()
+            return this.vars[variable] >= count;
+        }
+    }
+
+    if (this.vars[variable] != undefined) {
+        return this.vars[variable];
+    }
+
+    return 0;
+};
+
+/**
+ * сохранение
+ *
+ * @param {int} slot
+ */
+Quest.prototype.save = function(slot) {
+    var Datetime = new Date();
+
+    localStorage.setItem(this.name + '_' + slot.toString() + '_name', Datetime.toLocaleDateString() + ' ' + Datetime.toLocaleTimeString());
+    localStorage.setItem(this.name + '_' + slot.toString() + '_data', JSON.stringify({
+        items: this.items,
+        vars: this.vars,
+        position: this.position,
+        realCurrentLoc: this.realCurrentLoc
+    }));
+};
+
+/**
+ * загрузка
+ *
+ * @param {int} slot
+ */
+Quest.prototype.load = function(slot) {
+    var data = JSON.parse(localStorage.getItem(this.name + '_' + slot.toString() + '_data'));
+    this.items = data.items;
+    this.vars = data.vars;
+    this.position = data.position;
+    this.realCurrentLoc = data.realCurrentLoc;
+};
