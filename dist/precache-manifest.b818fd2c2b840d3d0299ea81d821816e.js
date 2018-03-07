d22143e4f024129fed9abe99bfa1ec3d515cf715
narmiel <tinycoyote@gmail.com>
2018-03-07 16:54:22 +0300

    build

---
+++ dist/precache-manifest.b818fd2c2b840d3d0299ea81d821816e.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cb2602639250e31227fec25ca219cc0",
+    "url": "./quests/Винни-Пух и другие звери.zip"
+  },
+  {
+    "revision": "a747ab643156baa1ed13007ba6980953",
+    "url": "./quests/Сара Фаерман 2.zip"
+  },
+  {
+    "revision": "1d8c08dcc5f6553652bf16d9e513b720",
+    "url": "./quests/Та девушка с четырнадцатого этажа и дождь.zip"
+  },
+  {
+    "revision": "6f87ed124c6e9a66a0cb23a934bf9f81",
+    "url": "./urql.html"
+  },
+  {
+    "revision": "fdf60411b29cb17cb6d7cc17292af7bd",
+    "url": "./quests/Хроники капитана Блуда Тайна левой ягодицы.zip"
+  },
+  {
+    "revision": "e26126f959dea4cb83f5860ee20f9931",
+    "url": "./quests/Холм фей.zip"
+  },
+  {
+    "revision": "755d8f0a6a9fb5710906606478cf7fe4",
+    "url": "./quests/Хозяйка Сэри.zip"
+  },
+  {
+    "revision": "edb14a66438ca580d55058fb4776c1c7",
+    "url": "./quests/Тень над Риверкроссом.zip"
+  },
+  {
+    "revision": "d37503a3928117afbd114f499a13c7c2",
+    "url": "./quests/Спойлер.zip"
+  },
+  {
+    "revision": "1b5523b04f20e5959772cc20ba3a3bac",
+    "url": "./quests/Маленькая зарисовка о больших чувствах.zip"
+  },
+  {
+    "revision": "ce2ca524aee3962ead0fa5fc9a515b07",
+    "url": "./quests/99999.zip"
+  },
+  {
+    "revision": "3df2a2516927d0d2ddc4c574de165b94",
+    "url": "./quests/Grunk and cheese.zip"
+  },
+  {
+    "revision": "b238b8fb9144a6b382b1b2c7d4cf00dc",
+    "url": "./quests/support.zip"
+  },
+  {
+    "revision": "eb763df14babec79241fcfb12299a496",
+    "url": "./quests/zolushka.zip"
+  },
+  {
+    "revision": "4a57387066517890a048bb9b40b2c064",
+    "url": "./quests/Адская суббота.zip"
+  },
+  {
+    "revision": "a6b22786541fb960998aee1cb133824b",
+    "url": "./quests/Mary Celeste.zip"
+  },
+  {
+    "revision": "997ac6dd833ad1c4a291e75bdb4b3928",
+    "url": "./quests/Баллада о рыцаре слова.zip"
+  },
+  {
+    "revision": "dd3638e094a1fd67b40227bb5d90b076",
+    "url": "./quests/Марс 2000.zip"
+  },
+  {
+    "revision": "e88054ea4405cf950dbdf68f9c43e604",
+    "url": "./quests/Дмитрий Сметанин-1 Начало.zip"
+  },
+  {
+    "revision": "07b630b57b665271ac52123da7d6a154",
+    "url": "./quests/Золотой Ключик, или как всё было на самом деле.zip"
+  },
+  {
+    "revision": "0cd77e437bdbe35f0fa4e6aaf21b7085",
+    "url": "./quests/Джинн из машины.zip"
+  },
+  {
+    "revision": "fe41b084f35d13a21c0a4fee57d6e6ca",
+    "url": "./quests/Латунный череп - Глава Первая - Быкомедведь.zip"
+  },
+  {
+    "revision": "0e64e72a2d1edd53d0e93e7022bf06db",
+    "url": "./quests/Alice's Adventures in Urqland.zip"
+  },
+  {
+    "revision": "550637651295450ac776022a0cb08314",
+    "url": "./quests/Как я стал пиратом.zip"
+  },
+  {
+    "revision": "f55d81047a59e3a4cc94f41cd313a744",
+    "url": "./quests/Похождения отважного хомяка Семена Побег.zip"
+  },
+  {
+    "revision": "7c65a34e8895d42385baaa9b2b9ce8d9",
+    "url": "./quests/Cursed City.zip"
+  },
+  {
+    "revision": "e353f7dac930aeccc6c1965ebb55ae52",
+    "url": "./plugins/drop_down_links.zip"
+  },
+  {
+    "revision": "1e67ef0a7e06160b0a368881888b61ae",
+    "url": "./plugins/disable_free_saves.zip"
+  },
+  {
+    "revision": "b2a1038743797feb4dd6",
+    "url": "./js/vendor.b2a10387.js"
+  },
+  {
+    "revision": "27e3dd7809fdcb1943e5",
+    "url": "./js/manifest.27e3dd78.js"
+  },
+  {
+    "revision": "1dcf9ce3c3be2c842e63",
+    "url": "./js/app.1dcf9ce3.js"
+  },
+  {
+    "revision": "0e16728aa3fd26ac72ca6b9ece947a16",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d749ec63ed56bd3f6b534810f7b1c7d",
+    "url": "./img/logo.1d749ec6.svg"
+  },
+  {
+    "revision": "fb39f43a5e8c8ee14252feaa9ec46a3a",
+    "url": "./games.json"
+  },
+  {
+    "revision": "1dcf9ce3c3be2c842e63",
+    "url": "./css/app.8c2b8313.css"
+  }
+];
