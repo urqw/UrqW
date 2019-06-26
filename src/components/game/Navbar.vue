e943f1b290a72f5610c82758bb789519718a713d
Konstantin Kitmanov <konstantin.kitmanov@fabres.pl>
2019-06-26 16:16:11 +0200

    рефактор навбара, иконки вынесены в отдельный компонент

--- src/components/game/Navbar.vue
+++ src/components/game/Navbar.vue
@@ -1,56 +1,51 @@
 <template>
-    <div>
-        <nav class="navbar is-light">
-            <div class="container">
-                <div class="navbar-brand">
-                    <a class="navbar-item" @click="clickBtn('home')">
-                        <Logo class="logo"/>
-                        <span>UrqW</span>
-                    </a>
-                </div>
-                <div class="navbar-menu">
-                    <div class="navbar-start">
-                        <a class="navbar-item" @click="clickBtn('switchVolume')">
-                            <span class="icon">
-                                <font-awesome-icon
-                                    :icon="volumeIcon"
-                                />
-                            </span>
-                        </a>
-                        <a class="navbar-item" @click="clickBtn('restartGame')">
-                        <span class="icon">
-                          <font-awesome-icon icon="sync" />
-                        </span>
-                        </a>
-                        <a :class="{ 'is-active': page === 'save' }"
-                           class="navbar-item"
-                           @click="clickBtn(page === 'save' ? 'returnToGame' : 'saveGame')"
-                        >
-                      <span class="icon">
-                        <font-awesome-icon icon="save" />
-                      </span>
-                        </a>
-                        <a :class="{ 'is-active': page === 'load' }"
-                           class="navbar-item"
-                           @click="clickBtn(page === 'load' ? 'returnToGame' : 'loadGame')"
-                        >
-                      <span class="icon">
-                        <font-awesome-icon icon="folder-open" />
-                      </span>
-                        </a>
-                    </div>
-                    <div class="navbar-end">
-                        <a class="navbar-item" @click="isOpenedSettings = !isOpenedSettings">
-                        <span class="icon">
-                          <font-awesome-icon icon="cog"/>
-                        </span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-        <SettingsPopup :open="isOpenedSettings" @cancel="isOpenedSettings = false"/>
-    </div>
+  <div>
+    <nav class="navbar is-light">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item" @click="clickBtn('home')">
+            <Logo class="logo" />
+            <span>UrqW</span>
+          </a>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-start">
+            <NavbarItem
+              :icon="volumeIcon"
+              @click.native="clickBtn('switchVolume')"
+            />
+            <NavbarItem icon="sync" @click.native="clickBtn('restartGame')" />
+            <NavbarItem
+              icon="save"
+              :title="$t('saveGame')"
+              :class="{ 'is-active': page === 'save' }"
+              @click.native="clickBtn(page === 'save' ? 'returnToGame' : 'saveGame')"
+            />
+            <NavbarItem
+              icon="folder-open"
+              :title="$t('loadGame')"
+              :class="{ 'is-active': page === 'load' }"
+              @click.native="clickBtn(page === 'load' ? 'returnToGame' : 'loadGame')"
+            />
+          </div>
+          <div class="navbar-end">
+            <a
+              class="navbar-item"
+              @click="isOpenedSettings = !isOpenedSettings"
+            >
+              <span class="icon">
+                <font-awesome-icon icon="cog" />
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <SettingsPopup
+      :open="isOpenedSettings"
+      @cancel="isOpenedSettings = false"
+    />
+  </div>
 </template>
 
 <script>
@@ -58,39 +53,39 @@ import Logo from "@/components/Logo";
 import SettingsPopup from "@/components/shared/SettingsPopup.vue";
 
 import { VOLUMES } from "@/const.js";
+import NavbarItem from "../shared/NavbarItem.vue";
 
 export default {
   name: "navbar",
   props: {
     Client: Object,
-    page: String,
+    page: String
   },
   data() {
     return {
       isOpenedSettings: false,
-      dropdownIsActive: false,
+      dropdownIsActive: false
     };
   },
   computed: {
     volumeIcon() {
       if (!this.Client) {
-        return 'volume-up';
+        return "volume-up";
       }
 
-      const volumeIndex = VOLUMES.findIndex(volume => volume === this.Client.volume);
+      const volumeIndex = VOLUMES.findIndex(
+        volume => volume === this.Client.volume
+      );
 
       if (volumeIndex === -1) {
-          return 'volume-up';
+        return "volume-up";
       }
 
-      return [
-        'volume-up',
-        'volume-down',
-        'volume-mute',
-      ][volumeIndex];
-    },
+      return ["volume-up", "volume-down", "volume-mute"][volumeIndex];
+    }
   },
   components: {
+    NavbarItem,
     SettingsPopup,
     Logo
   },
@@ -99,7 +94,7 @@ export default {
     clickBtn(name) {
       this.$emit("clickBtn", name);
     }
-  },
+  }
 };
 </script>
 
@@ -116,25 +111,25 @@ export default {
 }
 
 .navbar > .container {
-    display: flex;
+  display: flex;
 }
 
 .navbar-menu {
-    display: flex;
-    align-items: stretch;
-    flex-grow: 1;
-    flex-shrink: 0;
-    box-shadow: none;
-    background-color: whitesmoke;
-    color: #363636;
+  display: flex;
+  align-items: stretch;
+  flex-grow: 1;
+  flex-shrink: 0;
+  box-shadow: none;
+  background-color: whitesmoke;
+  color: #363636;
 }
 
 .navbar-start,
 .navbar-end {
-    display: flex;
+  display: flex;
 }
 
 .navbar-end {
-    margin-left: auto;
+  margin-left: auto;
 }
 </style>
