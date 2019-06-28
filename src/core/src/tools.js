308cea776d4d3f4e48d4581bf6e13608104fb7c0
narmiel <tinycoyote@gmail.com>
2019-06-28 19:26:54 +0300

    file encoding detection & convert

--- src/core/src/tools.js
+++ src/core/src/tools.js
@@ -51,35 +51,6 @@ export function isFloat(n) {
   return n === Number(n) && n % 1 !== 0;
 }
 
-const charmap = unescape(
-  "%u0402%u0403%u201A%u0453%u201E%u2026%u2020%u2021%u20AC%u2030%u0409%u2039%u040A%u040C%u040B%u040F" +
-    "%u0452%u2018%u2019%u201C%u201D%u2022%u2013%u2014%u0000%u2122%u0459%u203A%u045A%u045C%u045B%u045F" +
-    "%u00A0%u040E%u045E%u0408%u00A4%u0490%u00A6%u00A7%u0401%u00A9%u0404%u00AB%u00AC%u00AD%u00AE%u0407" +
-    "%u00B0%u00B1%u0406%u0456%u0491%u00B5%u00B6%u00B7%u0451%u2116%u0454%u00BB%u0458%u0405%u0455%u0457"
-);
-
-function code2char(code) {
-  if (code >= 0xc0 && code <= 0xff) {
-    return String.fromCharCode(code - 0xc0 + 0x0410);
-  }
-
-  if (code >= 0x80 && code <= 0xbf) {
-    return charmap.charAt(code - 0x80);
-  }
-
-  return String.fromCharCode(code);
-}
-
-export function win2unicode(str) {
-  let res = "";
-
-  for (let i = 0; i < str.length; i++) {
-    res = res + code2char(str.charCodeAt(i));
-  }
-
-  return res;
-}
-
 export function getExt(fileName) {
   return fileName
     .split(".")
