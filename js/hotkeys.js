9254d9a78bbd34d15619a8cd1a637df8d424d052
Nikita Tseykovets <tseikovets@rambler.ru>
2025-04-10 15:03:37 +0300

    Add cluster navigation with arrow keys

--- js/hotkeys.js
+++ js/hotkeys.js
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+// Active element when key down event
+var activeElementWhenKeyDown;
 // Object for storing key states
 var keyStates = {};
 // Object for storing key handlers
@@ -36,8 +38,35 @@ var allowedCombinations = [
     'Numpad7',
     'Numpad8',
     'Numpad9',
-    'Numpad0'
+    'Numpad0',
+    'ArrowLeft',
+    'ArrowUp',
+    'ArrowRight',
+    'ArrowDown'
 ];
+// Objects of navigation clusters
+var navigationClusters;
+
+// Code for cluster navigation that needs to be executed after page has loaded
+$(window).on("load", function() {
+    // Define array of cluster elements
+    navigationClusters = [
+    	$('.navbar-nav')[0],
+    	$('.navbar-right')[0],
+    	$('#textfield')[0],
+    	$('#buttons')[0],
+    	$('#input')[0]
+    ];
+    // Disable default Bootstrap handlers of arrow keys for inventory dropdown menu
+    $('#inventory').on('keydown', function(e) {
+        var keyCode = e.code || e.key;
+        if (keyCode && ['ArrowDown', 'ArrowUp'].includes(keyCode)) {
+            e.stopImmediatePropagation();
+            e.preventDefault();
+            return false;
+        }
+    });
+});
 
 // Function to register new hot key or combination
 function registerHotKey(combination, handler, allowDefault = false) {
@@ -50,6 +79,8 @@ function registerHotKey(combination, handler, allowDefault = false) {
 
 // Key down event handler
 document.addEventListener('keydown', (event) => {
+    activeElementWhenKeyDown = document.activeElement;
+
     // Not process hot keys if player is in waiting for any key to be pressed
     if (GlobalPlayer && GlobalPlayer.status == PLAYER_STATUS_ANYKEY) {
         return;
@@ -110,8 +141,7 @@ document.addEventListener('keyup', (event) => {
 }, true); // Use capture phase for maximum priority
 
 // Function to check if focus is within element
-function isFocusWithinElement(elementID) {
-    var $targetElement = $('#' + elementID);
+function isFocusWithinElement($targetElement) {
     var $activeElement = $(':focus');
     return $targetElement.find($activeElement).length > 0;
 }
@@ -119,11 +149,151 @@ function isFocusWithinElement(elementID) {
 // Function to activate button inside an element by number
 function activateButtonByNumber(elementID, number) {
     var element = document.getElementById(elementID);
-    if (!$('#' + elementID).is(':visible') || isFocusWithinElement('input')) return false;
+    if (!$('#' + elementID).is(':visible') || isFocusWithinElement($('#input'))) return false;
     var buttons = element.getElementsByTagName('button');
     if(number > buttons.length) return false;
     buttons[number - 1].click();
     return true;
+}
+
+// Function to check if element is text input field
+function isTextInput(element) {
+    return element.tagName.toLowerCase() === 'textarea' ||
+        (element.tagName.toLowerCase() === 'input' &&
+            ['text', 'password', 'email', 'search'].includes(element.type));
+}
+
+// Function to cluster navigation
+function ClusterNavigation(command) {
+    if (!navigationClusters) return false;
+
+    var activeElement = activeElementWhenKeyDown;
+    if (isTextInput(activeElement)) return false;
+
+    var clusters = navigationClusters.filter(item => {
+        var $element = $(item);
+        if (!$element.is(':visible')) {
+            return false;
+        }
+        if (getInteractiveChildren(item).length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if (clusters.length === 0) return false;
+
+    var indexOfCurrentCluster = getIndexOfCurrentCluster();
+    if (indexOfCurrentCluster < 0) {
+        // Focus first interactive element of first cluster
+        getInteractiveChildren(clusters[0])[0].focus();
+        return true;
+    }
+
+    switch(command) {
+        case 'ArrowRight':
+            return goToClusterByIndex(indexOfCurrentCluster + 1);
+        case 'ArrowLeft':
+            return goToClusterByIndex(indexOfCurrentCluster - 1);
+    }
+
+    var elementsOfCurrentCluster = getInteractiveChildren(clusters[indexOfCurrentCluster]);
+    var indexOfActiveElement = getIndexOfActiveElement();
+
+    switch(command) {
+        case 'ArrowDown':
+            return goToElementOfClusterByIndex(indexOfActiveElement + 1);
+        case 'ArrowUp':
+            return goToElementOfClusterByIndex(indexOfActiveElement - 1);
+        default:
+            return false;
+    }
+
+    // Returns array of child interactive elements
+    function getInteractiveChildren(element) {
+        var $visibleDescendants = $(element).find(':visible');
+        var $focusableElements = $visibleDescendants.filter(function() {
+            var $this = $(this);
+            var tagName = $this.prop('tagName').toLowerCase();
+    
+            var isDefaultFocusable = [
+                'input', 'select', 'textarea', 'button', 'a'
+            ].includes(tagName);
+            
+            var isEnabled = !$this.is(':disabled');
+            
+            var hasValidTabIndex = (
+                this.tabIndex >= 0 || 
+                ($this.attr('tabindex') !== undefined && $this.attr('tabindex') >= 0)
+            );
+            
+            var isLinkWithHref = ($this.is('a') && $this.attr('href') !== undefined);
+            
+            return (
+                (isDefaultFocusable && isEnabled) ||
+                hasValidTabIndex ||
+                isLinkWithHref
+            );
+        });
+        
+        return $focusableElements.get();
+    }
+
+    // Returns index of cluster in which active element is located
+    function getIndexOfCurrentCluster() {
+        for(var i = 0; i < clusters.length; i++) {
+            if (clusters[i].contains(activeElement)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Go to cluster with index closest to passed argument
+    function goToClusterByIndex(index) {
+        var length = clusters.length;
+        if (index > length) {
+            index = length;
+        } else if (index < 0) {
+            index = 0;
+        }
+        // Focus first interactive element of target cluster
+        getInteractiveChildren(clusters[index])[0].focus();
+        return true;
+    }
+
+    // Returns index of active element within current cluster
+    function getIndexOfActiveElement() {
+        for(var i = 0; i < elementsOfCurrentCluster.length; i++) {
+            if (elementsOfCurrentCluster[i] === activeElement) return i;
+        }
+        return -1;
+    }
+
+    // Go to element with index closest to passed argument
+    function goToElementOfClusterByIndex(index) {
+        /*
+         * Do not perform navigation on elements that open dropdown menus
+         * and/or are inside dropdown menus.
+         * They have their own implementation of keyboard navigation.
+         * Exception is inventory drop-down menu, where standard implementation must be replaced.
+         */
+        if (!document.getElementById('inventory').contains(activeElement)
+        && (activeElement.closest('.dropdown-toggle') !== null
+        || activeElement.closest('.dropdown-menu') !== null)) {
+            return false;
+        }
+        var length = elementsOfCurrentCluster.length;
+        if (index > length) {
+            index = length;
+        } else if (index < 0) {
+            index = 0;
+        }
+        elementsOfCurrentCluster[index].focus();
+        return true;
+    }
+
+    return false;
 }
 
 // Registering supported hot keys
@@ -137,7 +307,7 @@ registerHotKey('Escape', (event) => {
 }, true);
 
 registerHotKey('KeyA', (event) => {
-    if ($('#textfield').is(':visible') && !isFocusWithinElement('input')) {
+    if ($('#textfield').is(':visible') && !isFocusWithinElement($('#input'))) {
         var element = document.getElementById('textfield');
         var content = element.innerText.trim();
         var div = document.createElement('div');
@@ -163,7 +333,7 @@ registerHotKey('KeyI', (event) => {
         }
     }
 
-    if (!isFocusWithinElement('input')) {
+    if (!isFocusWithinElement($('#input'))) {
         $inventoryBtn.focus();
         if (!$inventoryBtn.parent().hasClass('open')) {
             $inventoryBtn.click();
@@ -174,7 +344,7 @@ registerHotKey('KeyI', (event) => {
 }, true);
 
 registerHotKey('KeyL', (event) => {
-    if ($('#load').is(':visible') && !isFocusWithinElement('input')) {
+    if ($('#load').is(':visible') && !isFocusWithinElement($('#input'))) {
         $('#load').click();
     } else {
         event.preventDefault(false);
@@ -182,7 +352,7 @@ registerHotKey('KeyL', (event) => {
 }, true);
 
 registerHotKey('KeyR', (event) => {
-    if ($('#restart').is(':visible') && !isFocusWithinElement('input')) {
+    if ($('#restart').is(':visible') && !isFocusWithinElement($('#input'))) {
         $('#restart').click();
     } else {
         event.preventDefault(false);
@@ -190,7 +360,7 @@ registerHotKey('KeyR', (event) => {
 }, true);
 
 registerHotKey('KeyS', (event) => {
-    if ($('#save').is(':visible') && !isFocusWithinElement('input')) {
+    if ($('#save').is(':visible') && !isFocusWithinElement($('#input'))) {
         $('#save').click();
     } else {
         event.preventDefault(false);
@@ -198,7 +368,7 @@ registerHotKey('KeyS', (event) => {
 }, true);
 
 registerHotKey('KeyV', (event) => {
-    if ($('#mute').is(':visible') && !isFocusWithinElement('input')) {
+    if ($('#mute').is(':visible') && !isFocusWithinElement($('#input'))) {
         $('#mute').click();
     } else {
         event.preventDefault(false);
@@ -321,6 +491,30 @@ registerHotKey('Numpad9', (event) => {
 
 registerHotKey('Numpad0', (event) => {
     if (!activateButtonByNumber('buttons', 10)) {
+        event.preventDefault(false);
+    }
+}, true);
+
+registerHotKey('ArrowLeft', (event) => {
+    if (!ClusterNavigation('ArrowLeft')) {
+        event.preventDefault(false);
+    }
+}, true);
+
+registerHotKey('ArrowUp', (event) => {
+    if (!ClusterNavigation('ArrowUp')) {
+        event.preventDefault(false);
+    }
+}, true);
+
+registerHotKey('ArrowRight', (event) => {
+    if (!ClusterNavigation('ArrowRight')) {
+        event.preventDefault(false);
+    }
+}, true);
+
+registerHotKey('ArrowDown', (event) => {
+    if (!ClusterNavigation('ArrowDown')) {
         event.preventDefault(false);
     }
 }, true);
