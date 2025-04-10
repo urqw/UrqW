d98bf816179f402ded03aeeb9cdb011ca6348e3d
Nikita Tseykovets <tseikovets@rambler.ru>
2025-04-10 15:20:42 +0300

    Delete UrqW Custom Cursor for Buttons

--- js/ctrl.js
+++ js/ctrl.js
@@ -214,59 +214,6 @@ $(function() {
             return;
         }
 
-        if (GlobalPlayer.status == PLAYER_STATUS_END) {
-            if (e.keyCode == 38 || e.keyCode == 40) {
-                var active = 0;
-                buttonField.find('button').each(function(index) {
-                    if ($(this).hasClass('active')) {
-                        active = index;
-                    }
-                });
-            }
-
-            var toActive;
-
-            if (e.keyCode == 38) {
-                toActive = active - 1;
-                if (toActive < 0) toActive = buttonField.find('button').length - 1;
-
-                buttonField.find('button').each(function(index) {
-                    if (index == toActive) {
-                        $(this).addClass('active');
-                    } else {
-                        $(this).removeClass('active');
-                    }
-                });
-            } else if (e.keyCode == 40) {
-                toActive = active + 1;
-                if (toActive > buttonField.find('button').length - 1) toActive = 0;
-
-                buttonField.find('button').each(function(index) {
-                    if (index == toActive) {
-                        $(this).addClass('active');
-                    } else {
-                        $(this).removeClass('active');
-                    }
-                });
-            }
-        }
-    });
-
-    $(document).keydown(function(e){
-        if (GlobalPlayer == null) {
-            return;
-        }
-
-        if (GlobalPlayer.status == PLAYER_STATUS_END) {
-            if (e.keyCode == 13) {
-                buttonField.find('button').each(function(index) {
-                    if ($(this).hasClass('active')) {
-                        $(this).click();
-                    }
-                });
-            }
-        }
-
         if (GlobalPlayer.status == PLAYER_STATUS_ANYKEY) {
             $('#info').hide();
             buttonField.show();
